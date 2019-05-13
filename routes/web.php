651050cc689613c62b2cb1942bb1e7d33868e002
Misha Rud <misha.rud@mail.ru>
2019-05-13 20:39:38 +0300

    rassian rigister

--- routes/web.php
+++ routes/web.php
@@ -20,6 +20,4 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
-Route::group(['prefix'=>'admin', 'namespace'=>'Admin','middleware'=>['auth']], function(){
-    Route::get('/', 'DashboardController@dashboard')->name('admin.index');
-});
+
